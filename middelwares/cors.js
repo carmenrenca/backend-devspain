30d7cb826ca3eb7223d64a2aea9481605cef7444
crendonDeloitte <crendon@deloitte.es>
2026-01-22 20:07:51 +0100

    añadido cors

--- middelwares/cors.js
+++ middelwares/cors.js
@@ -3,7 +3,13 @@ import { ACCEPTED_ORIGIN } from "../config.js";
 export const corsMiddelware = ({ acceptedOrigins = ACCEPTED_ORIGIN } = {}) => {
   return cors({
     origin: (origin, callback) => {
-      return callback(null, true);
+      if (!origin) return callback(null, true);
+
+      if (acceptedOrigins.includes(origin)) {
+        return callback(null, true);
+      }
+
+      return callback(new Error("Not allowed by CORS"));
     },
   });
 };
